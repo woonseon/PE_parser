ffe54d8ce7dd61d1dbbbaa96880109489ffb6390
woonseon <dbdnstjs8072@naver.com>
2018-01-22 17:46:29 +0900

    Make pe parser!

--- pe_parser.py
+++ pe_parser.py
@@ -6,48 +6,67 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
-def main(file_location):
-	pe = pefile.PE(file_location)
-
-	#print pe
-
-	#print pe.dump_info()
-
-	# IMAGE_DOS_HEADER 출력
+def total_pe(pe):
+	# total pe 출력
 	print "=============================================="
-	print pe.DOS_HEADER
+	print pe
 	print "=============================================="
 
+def DOS_HEADER(pe):
 	# IMAGE_DOS_HEADER 출력
 	print "=============================================="
 	print pe.DOS_HEADER
 	print "=============================================="	
 
+def NT_HEADERS(pe):
 	# NT HEADERS Signature 출력
 	print "=============================================="
 	print pe.NT_HEADERS
 	print "=============================================="
 
+def FILE_HEADER(pe):
 	# IMAGE_FILE_HEADER 출력
 	print "=============================================="
 	print pe.FILE_HEADER
 	print "=============================================="
+
+def NumberOfSections(pe):
 	# section number
+	print "=============================================="
 	print u"섹션 개수:"
 	print pe.FILE_HEADER.NumberOfSections
 	print "=============================================="
 
+def OPTIONAL_HEADER(pe):
 	print "=============================================="	
 	print pe.OPTIONAL_HEADER
 	print "=============================================="
 
+def Setion_Contents(pe):
 	print "=============================================="
-	print u"섹션 이름\t가상 주소(VA)"
+	print "OPTIONAL_HEADER"
 	for section in pe.sections:
-	    print section.Name + "\t" + hex(section.VirtualAddress)
+	    #print section.Name + "\t" + hex(section.VirtualAddress)
+	    print section
+	    print "=============================================="
 	print "=============================================="
-	#print pe.sections
+
+def select_menu():
+	print "\n***************************************************************"
+	print "***************************************************************"
+	print "Select Menu Please"
+	print "\t1. Show all PE contents"
+	print "\t2. Show DOS_HEADER"
+	print "\t3. Show NT_HEADERS"
+	print "\t4. Show FILE_HEADER"
+	print "\t5. Show Number Of Sections"
+	print "\t6. Show OPTIONAL_HEADER"
+	print "\t7. Show section contents"
+	print "\t8. Exit"
+	print "***************************************************************"
+	print "***************************************************************\n"
+	selection = raw_input("Select menu : ")
+	return selection
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -57,4 +76,24 @@ if __name__ == "__main__":
 		print "put only pe file"
 		sys.exit(1)
 
-	main(sys.argv[1])
+	pe = pefile.PE(sys.argv[1])
+
+	while True:
+		select = select_menu()
+		if select is '1':
+			total_pe(pe)
+		elif select is '2':
+			DOS_HEADER(pe)
+		elif select is '3':
+			NT_HEADERS(pe)
+		elif select is '4':
+			FILE_HEADER(pe)
+		elif select is '5':
+			NumberOfSections(pe)
+		elif select is '6':
+			OPTIONAL_HEADER(pe)
+		elif select is '7':
+			Setion_Contents(pe)
+		else:
+			print u"종료"
+			sys.exit(1)
